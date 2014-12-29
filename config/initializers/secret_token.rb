705e080a20ac4ce98747c9174d87ef5ae4387f9d
rocknrollmarc <marc.bluemner@gmail.com>
2014-12-29 13:53:24 +0100

    gem setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contestapp::Application.config.secret_key_base = '150d851cbc3b2eff23097ac3da565b05044b0dfebd53e82be61478dfb5d9b6df7ffdebff033f9a42a650aba4183159ab83537b96bde5325f115cb02d0fe2ca9c'
